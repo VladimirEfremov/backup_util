861cb703686df599016e885396905e19f91a99b3
VladimirEfremov <55531940+VladimirEfremov@users.noreply.github.com>
2020-01-20 21:12:32 +0400

    Add files via upload

--- backup_deploy.bat
+++ backup_deploy.bat
@@ -5,9 +5,11 @@ md proj_backup
 
 set /p pass=< pass.txt
 
-git clone "https://github.com/VladimirEfremov/proj_SI" proj_backup
+git clone "https://github.com/VladimirEfremov/proj_SI"  proj_backup
 
 cd proj_backup
 7z x proj.7z -oServiceMoq -p%pass%
-move ServiceMoq ../proj
+cd ServiceMoq
+move ServiceMoq ../../proj
+cd ..
 rmdir ServiceMoq
